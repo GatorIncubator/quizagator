562fb8d5a86702f4c63881c7a892b2180726bf5c
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2019-04-15 17:52:56 -0400

    Add needed route imports

--- application/__init__.py
+++ application/__init__.py
@@ -3,3 +3,10 @@ import flask
 
 # main flask instance
 app = flask.Flask(__name__)
+
+
+from . import index
+from . import db_connect
+from . import students
+from . import teachers
+from . import login
