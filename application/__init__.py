de96e10d5e292de97f9bd6b41e3aae20435fca5a
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2019-04-18 12:16:59 -0400

    Ignore import issues in __init__.py

--- application/__init__.py
+++ application/__init__.py
@@ -4,9 +4,9 @@ import flask
 # main flask instance
 app = flask.Flask(__name__)
 
-
-from . import index
-from . import db_connect
-from . import students
-from . import teachers
-from . import login
+# pylint: disable=wrong-import-position
+from . import index  # noqa: E402, F401
+from . import db_connect  # noqa: E402, F401
+from . import students  # noqa: E402, F401
+from . import teachers  # noqa: E402, F401
+from . import login  # noqa: E402, F401
