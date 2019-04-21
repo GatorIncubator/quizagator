2476a05c2e23b6063be9f62e973cdae1bbb89187
Jatt Mones <downhillsailor@gmail.com>
2019-04-21 16:07:23 -0400

    Adding a few sample files

---
+++ testCSVReader.py
@@ -0,0 +1,12 @@
+# Get: autoIncrementID, correct answer, topic_id, difficulty, question text, a answer_text, b answer_text, c_answer_text, d_answer_text, quizID
+# Given: Question, correct_answer number 0-3, answer a, b, c, d
+import csv
+#After user submits csv file
+file = open("sample.csv", "r")
+reader = csv.reader(file)
+questionArray = []
+quizID = 4
+for line in reader:
+    questionLine = '', line[0], line[1], line[2], line[3], line[4],line[5], quizID
+    questionArray.append(questionLine)
+print(questionArray)
