5a5fe114143e03c635f15c64cdf74618f385b97c
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2019-04-27 17:21:09 -0400

    First refactor of tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,26 +1,22 @@
-""" basic tests """
+"""Basic Tests"""
 import pytest
 
-from application import app
-from application import login
+from application import app as appfactory
 
+@pytest.fixture()
+def app_client() {
+    app = appfactory.create_app()
+
+    
+}
 
 def test_app_created():
     """Start with a blank database."""
     assert app is not None
 
 
-class FlaskrTestCase(unittest.TestCase):
-    def setUp(self):
-        self.db_fd, flaskr.app.config["DATABASE"] = tempfile.mkstemp()
-        flaskr.app.testing = True
-        self.app = flaskr.app.test_client()
-        with flaskr.app.app_context():
-            flaskr.init_db()
-
-
-# def test_index(app):
-#     res = app.get("/")
-#     # print(dir(res), res.status_code)
-#     assert res.status_code == 200
-#     assert b"" in res.data
+def test_index(app):
+    res = app.get("/")
+    # print(dir(res), res.status_code)
+    assert res.status_code == 200
+    assert b"" in res.data
