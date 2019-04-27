ae883656162f5ab187895e383859822dfef25154
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2019-04-27 17:39:38 -0400

    Add docstrings and pylint ignores in test_app
    Co-Authored by: yeej2 <yeej2@allegheny.edu>

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,20 +6,26 @@ from application import app as appfactory
 
 @pytest.fixture()
 def factory():
+    """Create an app and its test client"""
     actual_app = appfactory.create_app()
 
+    # pylint: disable=too-few-public-methods
     class Group:
+        """Group of created objects"""
+
         app = actual_app
         client = actual_app.test_client()
 
     return Group
 
 
+# pylint: disable=redefined-outer-name
 def test_app_created(factory):
     """Start with a blank database"""
     assert factory.app is not None
 
 
+# pylint: disable=redefined-outer-name
 def test_index(factory):
     """Test index page works -- NOT CORRECT"""
     res = factory.client.get("/")
