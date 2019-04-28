0854acea6d31de46606a9e024cf9d403199a6411
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2019-04-28 19:20:11 -0400

    Mount database as bound mount inside docker container

--- scripts/run-docker-image.sh
+++ scripts/run-docker-image.sh
@@ -3,6 +3,7 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 NAME="quizagator"
+DATA_FOLDER="$SCRIPT_DIR/../data"
 IMAGE_NAME="gatoreducator/quizagator"
 TAG="$(source $SCRIPT_DIR/version.sh)-dev"
 IMAGE="$IMAGE_NAME:$TAG"
@@ -13,4 +14,6 @@ OUTER_PORT="4201"
 docker stop "$NAME"
 docker rm "$NAME"
 
-docker run --name "$NAME" -e "FLASK_SECRET_KEY=dev" -p "${OUTER_PORT}:${INNER_PORT}" "$IMAGE"
+docker run -e "FLASK_SECRET_KEY=dev" -p "${OUTER_PORT}:${INNER_PORT}" \
+    --name "$NAME" --mount "type=bind,source=$DATA_FOLDER,target=/data" \
+    "$IMAGE"
