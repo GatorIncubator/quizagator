3378ae0f537453b4f57711f08b50c278126f7ac8
Andrew Everitt <31443695+everitt-andrew@users.noreply.github.com>
2019-04-29 19:30:04 -0400

    Update testCSVReader.py

--- testCSVReader.py
+++ testCSVReader.py
@@ -1,12 +1,13 @@
-# Get: autoIncrementID, correct answer, topic_id, difficulty, question text, a answer_text, b answer_text, c_answer_text, d_answer_text, quizID
+# Get: autoIncrementID, correct answer, topic_id, difficulty, question text, a answer_text,
+# Get (cont.): b answer_text, c_answer_text, d_answer_text, quizID
 # Given: Question, correct_answer number 0-3, answer a, b, c, d
 import csv
-#After user submits csv file
+# After user submits csv file
 file = open("sample.csv", "r")
 reader = csv.reader(file)
 questionArray = []
 quizID = 4
 for line in reader:
-    questionLine = '', line[0], line[1], line[2], line[3], line[4],line[5], quizID
+    questionLine = '', line[0], line[1], line[2], line[3], line[4], line[5], quizID
     questionArray.append(questionLine)
 print(questionArray)
