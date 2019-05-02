26f518b3b1c703ec6a92470ee36d8646e2e79ff5
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2019-05-02 04:02:32 -0400

    Add skeleton grade.py

---
+++ application/grade.py
@@ -0,0 +1,7 @@
+"""Grade student responses by handing off determinations to user code"""
+
+
+def grade(student_id, quiz_id):
+    """Grade a single student's quiz"""
+    print(f"Graded student {student_id}'s quiz {quiz_id}!")
+    return "100%"
